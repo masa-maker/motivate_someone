0873c0729ba6235242864e8fab36b43ba12f9feb
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-14 16:05:50 +0900

    投稿一覧画面実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
   end
 
   def create
+  end
+
+  def post_params
+    params.require(:post).permit(:text, :image)
   end
 end
